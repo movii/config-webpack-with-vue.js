dd1f6fa270240f7547d585ba98bfa63883421a75
Lien <mo.strangeline@gmail.com>
2018-06-17 14:19:05 +0800

    add: resolve vue$ with vue.esm.js

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,15 @@
 let path = require('path');
 
 module.exports = {
-  entry: './src/js/app.js',
   mode: 'development',
+  entry: './src/js/app.js',
   output: {
     path: path.resolve(__dirname, 'dist'),
     filename: 'bundle.js'
+  },
+  resolve: {
+    alias: {
+     'vue$': 'vue/dist/vue.esm.js'
+     }
   }
 };
