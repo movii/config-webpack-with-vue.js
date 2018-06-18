8a0e95691f195c341e82523bda39bf39a142aa7e
Lien <mo.strangeline@gmail.com>
2018-06-18 10:52:12 +0800

    add: counter.vue as HMR exmaple

--- src/js/App.vue
+++ src/js/App.vue
@@ -1,11 +1,17 @@
 <template>
-  <h1>Hello World with webpack-dev-server</h1>
+  <div>
+    <h1>Hello World with webpack-dev-server</h1>
+    <counter></counter>
+  </div>
 </template>
 
 <script>
+import counter from './counter.vue'
+
 export default {
   data () {
     return {}
-  }
+  },
+  components: { counter }
 }
 </script>
---
+++ src/js/counter.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+    <div> {{ count }}</div>
+    <button @click="count++">计数器 + 1</button>
+  </div>
+</template>
+
+<script>
+export default {
+  data () { return { count: 0 } }
+}
+</script>
+
